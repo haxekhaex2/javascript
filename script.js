62fb1ddbea4b5ce745f1d74c314de090bdd68248
haxekhaex2 <haxekhaex2@gmail.com>
2024-01-24 21:40:05 -0500

    javascript fundamentals

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-alert("Hello, world!");
+"use strict";
+let result = prompt("Hello, world!", "Woah...");
+let message = 'Hello!';
+confirm("Hello!");
+alert(message);
+
+let a = 1, b = 1;
+let c = ++a;
+let d = b++;
+
+let a = 2;
+let x = 1 + (a *= 2);
+
