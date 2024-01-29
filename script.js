c31c45a17df813ae088cc376c564ad0b7ef5c63d
haxekhaex2 <haxekhaex2@gmail.com>
2024-01-29 16:59:03 -0500

    strings

--- script.js
+++ script.js
@@ -1,44 +1,34 @@
 "use strict";
 
-/* Sum numbers from visitor. */
-let foo = Number(prompt("Please insert a number.", 10));
-let bar = Number(prompt("Please insert another number.", 20));
-alert(foo + bar);
-
-/* toFixed(). */
-alert(1.35.toFixed(1)); /* 1.3 */
-alert(6.35.toFixed(1)); /* 1.4 */
-
-/* Repeat until input is a number. */
-function readNumber(){
-	let number;
-	
-	do{
-		number = prompt("Enter a number, please.", 0);
-		if(number == "CANCEL" || number == "") return null;
-	}while(!isFinite(number));
-	
-	if(number === null || number === "") return null;
-	
-	return Number(number);
+/* Uppercase the first character. */
+function ucFirst(string){
+	if(string.length > 0) return string[0].toUpperCase() + string.slice(1); else return "";
 }
 
-alert(readNumber());
+alert(ucFirst("aaa"));
 
-/* A random number from min to max. */
-function random(min, max){
-	return Math.random() * (max - min) + min;
+/* Check for spam. */
+function checkSpam(string){
+	let lowerCase = string.toLowerCase();
+	return lowerCase.includes("viagra") || lowerCase.includes("xxx");
 }
 
-alert(random(1, 5));
-alert(random(1, 5));
-alert(random(1, 5));
-
-/* A random integer from min to max. */
-function randomInteger(min, max){
-	return parseInt(Math.floor(Math.random() * (max + 1 - min) + min));
+/* Truncate the text. */
+function truncate(string, maxLength){
+	if(string.length > maxLength){
+		if(maxLength < 3){
+			return string.substring(0, 3);
+		}else{
+			let truncated = string.substring(0, maxLength - 3);
+			truncated += "...";
+			return truncated;
+		}
+	}
+	
+	return string;
 }
 
-alert(random(1, 5));
-alert(random(1, 5));
-alert(random(1, 5));
+/* Extract the money. */
+function extractCurrencyValue(string){
+	return Number(string.slice(1));
+}
