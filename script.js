064e6b44e9e6d0c2c6ad598d1e1d7c53eb2a8b5b
haxekhaex2 <haxekhaex2@gmail.com>
2024-01-29 14:54:12 -0500

    methods of primitives

--- script.js
+++ script.js
@@ -1,45 +1,10 @@
 "use strict";
 
-// Using "this" in an object literal.
-function makeUser(){
-	return {name: "John", ref: this};
-}
+/* Adding a string. */
+let str = "Hello";
+str.test = 5;
+alert(str.test); /* Error. */
 
-let user = makeUser();
-alert(user.ref.name); // Undefined access.
-
-// Create a calculator.
-let calculator = new Object();
-calculator.read = function(){
-	this.a = Number(prompt("Value of a:", 10));
-	this.b = Number(prompt("Value of b:", 20));
-};
-calculator.sum = function(){
-	return this.a + this.b;
-};
-calculator.mul = function(){
-	return this.a * this.b;
-};
-
-calculator.read();
-alert(calculator.sum());
-alert(calculator.mul());
-
-// Chaining.
-let ladder = {
-	step: 0,
-	up(){
-		this.step++;
-		return this;
-	},
-	down(){
-		this.step--;
-		return this;
-	},
-	showStep: function(){
-		alert(this.step);
-		return this;
-	}
-};
-
-ladder.up().up().down().showStep().down().showStep();
+/* A wrapper object is created to access str.test. */
+/* Writing into it is an error in strict mode. */
+/* In non-strict mode, the write occurs, but the object is unwrapped back into a string, and str.test disappears. */
