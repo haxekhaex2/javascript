416735c8fbaef88b470cad8d1172dc18b869c5b5
haxekhaex2 <haxekhaex2@gmail.com>
2024-02-07 21:25:22 -0500

    function objects

--- script.js
+++ script.js
@@ -1,38 +1,35 @@
 "use strict";
 
-/* Sum with closures. */
+/* Set and decrease for counter. */
+function counter(){
+	counter.number = 0;
+	
+	counter.set = function(x){
+		counter = x;
+	}
+	
+	counter.decrease = function(x){
+		return counter.number--;
+	}
+	
+	return counter.number++;
+	
+}
+
+/* Sum with an arbitrary amount of brackets. */
 function sum(a){
-	return function(b){
-		return a + b;
-	};
-}
-
-/* Filter through function. */
-let arr = [1, 2, 3, 4, 5, 6, 7];
-
-function inBetween(a, b){
-	return function(element){
-		return index >= a && index < b;
+	let currentSum = a;
+	
+	function f(b){
+		currentSum += b;
+		return f;
 	}
-}
-
-function inArray(givenElements){
-	return function(element){
-		return givenElements.includes(element);
-	};
-}
-
-alert( arr.filter(inArray([1, 2, 10])) );
-
-/* Sort by field. */
-let users = [
-  { name: "John", age: 20, surname: "Johnson" },
-  { name: "Pete", age: 18, surname: "Peterson" },
-  { name: "Ann", age: 19, surname: "Hathaway" }
-];
-
-function byField(field){
-	return function(a, b){
-		return a.field > b.field ? 1 : -1;
+	
+	f.toString = function(){
+		return currentSum;
 	}
+	
+	return f;
 }
+
+alert(sum(0)(1)(2)(3)(4)(5));
