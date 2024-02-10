4766157e7ec90c9e6446a9d8a0c85304d293e164
haxekhaex2 <haxekhaex2@gmail.com>
2024-02-09 21:32:10 -0500

    function decorators and forwarding

--- script.js
+++ script.js
@@ -1,11 +1,54 @@
 "use strict";
 
-/* Print numbers. */
-function printNumbers(from, to){
-	console.log(from);
-	if(from != to) setTimeout(() => {printNumbers(from + 1, to);}, 1000);
+/* Spy decorator. */
+function spy(func){
+	function wrapper(...args){
+		wrapper.calls.push(args);
+		return func.apply(this, args);
+	}
+
+	wrapper.calls = [];
+
+	return wrapper;
 }
 
-function printNumbersSetInterval(from, to){
-	let timerId = setInterval((from, to) => {if(from === to){clearInterval(timerId); return;} console.log(from, to); from++;});
+/* Delaying decorator. */
+function delay(func, ms){
+	return function(){
+		setTimeout(() => {func.apply(this, argruments);}, ms);
+	};
+}
+
+/* Debounce decorator. */
+function debounce(f, ms){
+	let timeoutHandle = null;
+
+	return function(){
+		clearTimeout(timeoutHandle);
+		timeoutHandle = setTimeout(() => {
+			f.apply(this, arguments);
+		}, ms);
+	}
+}
+
+/* Throttle decorator. */
+function throttle(f, ms){
+	let counter = 0;
+	let timeoutHandle = null;
+	let thisArray = [];
+	let argumentsArray = [];
+	return function(){
+		counter++;
+		thisArray.push(this);
+		argumentsArray.push(arguments);
+		if(!timeoutHandle) timeoutHandle = setInterval(() => {
+			if(counter === 0){
+				clearTimeout(timeoutHandle);
+				timeoutHandle = null;
+				return;
+			}
+			f.apply(thisArray.shift(), argumentsArray.shift());
+			counter--;
+		}, ms);
+	}
 }
