0164f0275c8f9cdc59bb293138d1349c84913960
haxekhaex2 <haxekhaex2@gmail.com>
2024-02-12 20:23:26 -0500

    prototypal inheritance

--- script.js
+++ script.js
@@ -1,39 +1,22 @@
 "use strict";
 
-/* Fix a function that loses "this". */
-function askPassword(ok, fail){
-	let password = prompt("Password?", "");
-	if(password == "rockstar") ok();
-	else fail();
-}
-
-let user = {
-	name: "John",
-
-	loginOk(){
-		alert(`${this.name} logged in.`);
-	},
-
-	loginFail(){
-		alert(`${this.name} failed to log in.`);
-	}
+/* Searching algorithm. */
+let head = {
+	glasses: 1,
 };
 
-askPassword(user.loginOk.bind(user), user.loginFail.bind(user));
+let table = {
+	pen: 3,
+	__proto__: head
+};
 
-/* Partial application for login. */
-function askPassword(ok, fail){
-	let password = prompt("Password?", "");
-	if(password == "rockstar") ok();
-	else fail();
-}
+let bed = {
+	sheet: 1,
+	pillow: 2,
+	__proto__: table
+};
 
-user = {
-	name: "John",
-
-	login(result){
-		alert(this.name + (result ? "Logged in." : "Failed to log in."));
-	}
-}
-
-askPassword(user.login.bind(user, true), user.login.bind(user, false));
+let pockets = {
+	money: 2000,
+	__proto__: bed
+};
