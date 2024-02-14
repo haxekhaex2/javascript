baf941486fb0c75f69baebcce0ba497d1c40d33e
haxekhaex2 <haxekhaex2@gmail.com>
2024-02-14 18:15:18 -0500

    class basic syntax

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
 "use strict";
 
-/* Add toString to the dictionary. */
-let dictionary = Object.create(null);
+/* Rewrite to class. */
+class Clock{
+	constructor(template){
+		this.timer = null;
+		this.templace = template;
+	}
 
-Object.defineProperty(dictionary, "toString", {
-	value: function(){
-		return Object.keys(this).join(", ");
-	},
-	enumerable: false
-});
+	stop(){
+		clearInterval(this.timer);
+	}
 
-dictionary.apple = "Apple";
-dictionary.__proto__ = "__test__"; /* __proto__ is a regular property key here. */
+	start(){
+		this.render();
+		this.timer = setInterval(() => {this.render();}, 1000);
+	}
+
+	render(){
+		let date = new Date();
+		let hours = date.getHours();
+		if(hours < 10) hours = '0' + hours;
+		let mins = date.getMinutes();
+		if(mins < 10) mins = '0' + minutes;
+		let secs = date.getSeconds();
+		if(secs < 10) secs = '0' + seconds;
+
+		let output = this.template.replace('h', hours).replace('m', minutes).replace('s', secs);
+	}
+}
+
+let clock = new Clock({template: "h:m:s"});
+clock.start();
