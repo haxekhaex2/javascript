b394818c496fd51cbecfac57321c3637ed6458b8
haxekhaex2 <haxekhaex2@gmail.com>
2024-02-15 11:04:25 -0500

    static properties and methods

--- script.js
+++ script.js
@@ -1,31 +1,12 @@
 "use strict";
 
-/* Error creating an instance. */
-class Animal{
+/* Class extends Object. */
+class Rabbit extends Object{
 	constructor(name){
+		super();
 		this.name = name;
 	}
 }
 
-class Rabbit extends Animal{
-	constructor(name){
-		super(name);
-		this.created = Date.now();
-	}
-}
-
-let rabbit = new Rabbit("White Rabbit");
-alert(rabbit.name);
-
-/* Extended clock. */
-class ExtendedClock extends Clock{
-	constructor({template}, precision){
-		super(template);
-		this.precision = precision;
-	}
-	
-	start(){
-		this.render();
-		this.timer = setInterval(() => this.render(), this.precision);
-	}
-}
+let rabbit = new Rabbit("Rab");
+alert(rabbit.hasOwnProperty("name"));
