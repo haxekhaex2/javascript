3fae87c5b2f7f9ec9f5a9c6da83f0246f272a558
haxekhaex2 <haxekhaex2@gmail.com>
2024-02-14 23:48:17 -0500

    class inheritance

--- script.js
+++ script.js
@@ -1,33 +1,31 @@
 "use strict";
 
-/* Rewrite to class. */
-class Clock{
-	constructor(template){
-		this.timer = null;
-		this.templace = template;
-	}
-
-	stop(){
-		clearInterval(this.timer);
-	}
-
-	start(){
-		this.render();
-		this.timer = setInterval(() => {this.render();}, 1000);
-	}
-
-	render(){
-		let date = new Date();
-		let hours = date.getHours();
-		if(hours < 10) hours = '0' + hours;
-		let mins = date.getMinutes();
-		if(mins < 10) mins = '0' + minutes;
-		let secs = date.getSeconds();
-		if(secs < 10) secs = '0' + seconds;
-
-		let output = this.template.replace('h', hours).replace('m', minutes).replace('s', secs);
+/* Error creating an instance. */
+class Animal{
+	constructor(name){
+		this.name = name;
 	}
 }
 
-let clock = new Clock({template: "h:m:s"});
-clock.start();
+class Rabbit extends Animal{
+	constructor(name){
+		super(name);
+		this.created = Date.now();
+	}
+}
+
+let rabbit = new Rabbit("White Rabbit");
+alert(rabbit.name);
+
+/* Extended clock. */
+class ExtendedClock extends Clock{
+	constructor({template}, precision){
+		super(template);
+		this.precision = precision;
+	}
+	
+	start(){
+		this.render();
+		this.timer = setInterval(() => this.render(), this.precision);
+	}
+}
