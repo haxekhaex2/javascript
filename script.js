57c7f2530792bd2aab81b02f213c0b7c7a5251bc
haxekhaex2 <haxekhaex2@gmail.com>
2024-02-16 20:39:31 -0500

    custom errors

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 "use strict";
 
-/* Finally or just the code? */
-try{
-	/* Here, perform an operation that may or may not result in an error. */
-	throwError(); /* No such function throwError(). */
-}catch(error){
-	if(!(error instanceof SyntaxError)) throw(error); /* In the case that it wasn't a syntax error, rethrow it. */
-	console.log(error); /* In the case that it was, log it. */
-}finally{
-	/* If an error was rethrown in the previous catch block, then this finally clause is needed to do cleanup. */
-	doCleanupForTheTryBlock();
+/* Inherit from SyntaxError. */
+class FormatError extends SyntaxError{
+	constructor(message){
+		super(message);
+		this.name = this.constructor.name;
+	}
 }
