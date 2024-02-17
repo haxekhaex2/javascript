17b930bffe9b44326fbaaa75e84b47753f92aa3d
haxekhaex2 <haxekhaex2@gmail.com>
2024-02-16 20:18:22 -0500

    error handling

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 "use strict";
 
-/* Class extends Object. */
-class Rabbit extends Object{
-	constructor(name){
-		super();
-		this.name = name;
-	}
+/* Finally or just the code? */
+try{
+	/* Here, perform an operation that may or may not result in an error. */
+	throwError(); /* No such function throwError(). */
+}catch(error){
+	if(!(error instanceof SyntaxError)) throw(error); /* In the case that it wasn't a syntax error, rethrow it. */
+	console.log(error); /* In the case that it was, log it. */
+}finally{
+	/* If an error was rethrown in the previous catch block, then this finally clause is needed to do cleanup. */
+	doCleanupForTheTryBlock();
 }
-
-let rabbit = new Rabbit("Rab");
-alert(rabbit.hasOwnProperty("name"));
