088b33dcc59c3123f62a0dea2fa7c7b8585f6cf0
haxekhaex2 <haxekhaex2@gmail.com>
2024-02-19 17:13:19 -0500

    promises

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
 "use strict";
 
-/* Inherit from SyntaxError. */
-class FormatError extends SyntaxError{
-	constructor(message){
-		super(message);
-		this.name = this.constructor.name;
-	}
+/* Delay with a promise. */
+function delay(ms){
+	return new Promise(function(resolve, reject){
+		setTimeout(() => resolve(), ms);
+	});
+}
+
+delay(3000).then(() => alert("runs after 3 seconds"));
+
+/* Animated circle with promise. */
+function showCircle(cx, cy, radius, callback) {
+	let div = document.createElement('div');
+	div.style.width = 0;
+	div.style.height = 0;
+	div.style.left = cx + 'px';
+	div.style.top = cy + 'px';
+	div.className = 'circle';
+	document.body.append(div);
+
+	return new Promise(function(resolve, reject){
+		setTimeout(() => {
+			div.style.width = radius * 2 + 'px';
+			div.style.height = radius * 2 + 'px';
+
+			div.addEventListener('transitionend', function handler() {
+				div.removeEventListener('transitionend', handler);
+				resolve(div);
+			});
+		}, 0);
+	});
 }
