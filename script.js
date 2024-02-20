380a5357b1a7dd22cd7abb7fb759f28ea9a6fbbf
haxekhaex2 <haxekhaex2@gmail.com>
2024-02-20 11:24:17 -0500

    async and await

--- script.js
+++ script.js
@@ -1,33 +1,67 @@
 "use strict";
 
-/* Delay with a promise. */
-function delay(ms){
-	return new Promise(function(resolve, reject){
-		setTimeout(() => resolve(), ms);
-	});
+/* Rewrite using async/await. */
+async function loadJson(url){
+	let header = await fetch(url);
+	if(!header.status === 200) throw new Error(header.status);
+	let json = await header.json();
+	return json;
 }
 
-delay(3000).then(() => alert("runs after 3 seconds"));
+loadJson('https://javascript.info/no-such-user.json').catch(alert); 
 
-/* Animated circle with promise. */
-function showCircle(cx, cy, radius, callback) {
-	let div = document.createElement('div');
-	div.style.width = 0;
-	div.style.height = 0;
-	div.style.left = cx + 'px';
-	div.style.top = cy + 'px';
-	div.className = 'circle';
-	document.body.append(div);
-
-	return new Promise(function(resolve, reject){
-		setTimeout(() => {
-			div.style.width = radius * 2 + 'px';
-			div.style.height = radius * 2 + 'px';
-
-			div.addEventListener('transitionend', function handler() {
-				div.removeEventListener('transitionend', handler);
-				resolve(div);
-			});
-		}, 0);
-	});
+/* Rewrite  "rethrow" with async/await. */
+class HttpError extends Error {
+	constructor(response) {
+		super(`${response.status} for ${response.url}`);
+		this.name = 'HttpError';
+		this.response = response;
+	}
 }
+
+function loadJson(url) {
+	return fetch(url)
+		.then(response => {
+			if (response.status == 200) {
+				return response.json();
+			} else {
+				throw new HttpError(response);
+			}
+		});
+}
+
+// Ask for a user name until github returns a valid user
+async function demoGithubUser() {
+	let user;
+	while(true){
+		let name = prompt("Enter a name?", "iliakan");
+		
+		try{
+			user = await loadJson(`https://api.github.com/users/${name}`);
+			break;
+		}catch(err){
+			if(err instanceof  HttpError && err.response.status === 404){
+				alert("No such user, please reenter.");
+			}else{
+				throw err;
+			}
+		}
+	}
+	
+	alert(`Full name: ${user.name}.`);
+	return user;
+}
+
+demoGithubUser();
+
+/* Call async from non-async. */
+async function wait(){
+	await new Promise(resolve => setTimeout(resolve, 1000));
+	return 10;
+}
+
+function f(){
+	wait().then(value => alert(value));
+}
+
+f();
