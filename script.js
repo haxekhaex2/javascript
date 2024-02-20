bffdad2cd1605d4a2a12839db1a062fe910281c5
haxekhaex2 <haxekhaex2@gmail.com>
2024-02-20 13:47:55 -0500

    generators

--- script.js
+++ script.js
@@ -1,67 +1,16 @@
 "use strict";
 
-/* Rewrite using async/await. */
-async function loadJson(url){
-	let header = await fetch(url);
-	if(!header.status === 200) throw new Error(header.status);
-	let json = await header.json();
-	return json;
-}
-
-loadJson('https://javascript.info/no-such-user.json').catch(alert); 
-
-/* Rewrite  "rethrow" with async/await. */
-class HttpError extends Error {
-	constructor(response) {
-		super(`${response.status} for ${response.url}`);
-		this.name = 'HttpError';
-		this.response = response;
-	}
-}
-
-function loadJson(url) {
-	return fetch(url)
-		.then(response => {
-			if (response.status == 200) {
-				return response.json();
-			} else {
-				throw new HttpError(response);
-			}
-		});
-}
-
-// Ask for a user name until github returns a valid user
-async function demoGithubUser() {
-	let user;
+/* Pseudo-random generator. */
+function* psuedoRandom(seed){
+	let value = seed;
 	while(true){
-		let name = prompt("Enter a name?", "iliakan");
-		
-		try{
-			user = await loadJson(`https://api.github.com/users/${name}`);
-			break;
-		}catch(err){
-			if(err instanceof  HttpError && err.response.status === 404){
-				alert("No such user, please reenter.");
-			}else{
-				throw err;
-			}
-		}
+		let value = value * 16807 % 2147483647;
+		yield value;
 	}
-	
-	alert(`Full name: ${user.name}.`);
-	return user;
 }
 
-demoGithubUser();
+let generator = pseudoRandom(1);
 
-/* Call async from non-async. */
-async function wait(){
-	await new Promise(resolve => setTimeout(resolve, 1000));
-	return 10;
-}
-
-function f(){
-	wait().then(value => alert(value));
-}
-
-f();
+alert(generator.next().value); // 16807
+alert(generator.next().value); // 282475249
+alert(generator.next().value); // 1622650073
