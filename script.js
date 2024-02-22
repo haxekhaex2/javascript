73c24ff094fd24b34c11ab018641fce8e5cea425
haxekhaex2 <haxekhaex2@gmail.com>
2024-02-21 22:55:04 -0500

    proxy and reflect

--- script.js
+++ script.js
@@ -1,16 +1,64 @@
 "use strict";
 
-/* Pseudo-random generator. */
-function* psuedoRandom(seed){
-	let value = seed;
-	while(true){
-		let value = value * 16807 % 2147483647;
-		yield value;
-	}
+/* Error on reading non-existent property. */
+let user = {
+	name: "John"
+};
+
+function wrap(target){
+	return new Proxy(target, {
+		get(target, property, receiver){
+			if(property in target) throw new Error("No such property.");
+			return Reflect.get(target, property, receiver);
+		}
+	});
 }
 
-let generator = pseudoRandom(1);
+user = wrap(user);
 
-alert(generator.next().value); // 16807
-alert(generator.next().value); // 282475249
-alert(generator.next().value); // 1622650073
+alert(user.name);
+alert(user.age);
+
+/* Accessing array[-1]. */
+let array = [1, 2, 3];
+
+array = new Proxy(array, {
+	get(target, property, receiver){
+		if(Math.sign(property) === -1) property += target.length;
+		Reflect.get(target, property, receiver);
+	},
+	set(target, property, value, receiver){
+		if(Math.sign(property) === -1) property += target.length;
+		Reflect.set(target, property, value, receiver);
+		return true;
+	}
+});
+
+array[-1];
+array[-2];
+array[-3];
+
+/* Observable. */
+function makeObservable(target){
+	target.observe = function(handler){
+		this.handler = handler;
+	};
+	
+	return new Proxy(target, {
+		
+		set(target, property, value, receiver){
+			if(target.handler) handler(property, value);
+			Reflect.set(target, property, value, receiver);
+			return true;
+		}
+	});
+}
+
+let user = {};
+user = makeObservable(target);
+
+user.observe((key, value) => {
+	alert(`SET ${key}=${value}`);
+});
+
+user.name = "John";
