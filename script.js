ae1c6c23582a24eaea55cc2311a11b55b30a264b
haxekhaex2 <haxekhaex2@gmail.com>
2024-02-28 23:12:41 -0500

    basic dom node properties

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 "use strict";
 
-/* Search for elements. */
-let table = document.getElementById("age-table");
-let labels = table.querySelectorAll("label");
-let tableData = table.querySelector("td");
-// let form = Array.from(document.querySelectorAll("form")).find(function(value, element, array){if(value.getAttribute("name") === "search") return true; return false;});
-let form = document.getElementsByName("search")[0];
-let firstInput = docmument.getElementByTagName("input")[0];
-let lastInput = form.querySelectorAll("input").slice(-1).pop();
+/* Count descendants. */
+for(let listItem in document.querySelectorAll("li")){
+    console.log(listItem.data + Array.from(listItem.querySelectorall("li")).length);
+}
