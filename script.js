a5bed2a9eba8f799349beac78cc3f23d76f32bb0
haxekhaex2 <haxekhaex2@gmail.com>
2024-02-29 23:44:51 -0500

    modifying the document

--- script.js
+++ script.js
@@ -4,3 +4,54 @@
 for(let listItem in document.querySelectorAll("li")){
     console.log(listItem.data + Array.from(listItem.querySelectorall("li")).length);
 }
+
+/* Clear the element. */
+function clear(elem) {
+	elem.innerHTML = "";
+}
+
+/* Create a list. */
+function createList(){
+	let ul = document.createElement("ul");
+	let pr;
+	while(pr = prompt("Enter list item text.", "Put text here."){
+		let li = document.createElement("li");
+		li.innerText = pr;
+		ul.append(li);
+	}
+	return ul;
+}
+
+/* Create a tree. */
+let data = {
+  "Fish": {
+    "trout": {},
+    "salmon": {}
+  },
+
+  "Tree": {
+    "Huge": {
+      "sequoia": {},
+      "oak": {}
+    },
+    "Flowering": {
+      "apple tree": {},
+      "magnolia": {}
+    }
+  }
+};
+
+createTree(data);
+
+function createTree(data){
+	let ul = document.createElement("ul");
+	for(let property of Object.keys(data)){
+		let li = document.createElement("li");
+		li.innerText = property;
+		li.appendChild(createTree(data[property]));
+		ul.appendChild(li);
+	}
+	return ul;
+}
+
+
